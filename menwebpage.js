c7a47a6db92ff1afe420d213a22a7dfd34f5e581
ayeshas21 <65433905+ayeshas21@users.noreply.github.com>
2020-06-04 19:38:53 -0600

    Add files via upload

---
+++ menwebpage.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function myFunction() {
+  var txt;
+  if (confirm("You have subscribed")) {
+    txt = "You pressed OK!";
+  } 
+  document.getElementById("demo").innerHTML = txt;
+}
